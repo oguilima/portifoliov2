1c44af3c4d87dde8ca634bae475f20b48edfde01
oguilima <oficialguilima@gmail.com>
2025-05-05 00:06:04 -0300

    inclusão de loading para busca dos projetos

--- src/pages/projetos/projetos.component.ts
+++ src/pages/projetos/projetos.component.ts
@@ -6,21 +6,25 @@ import { CarouselModule } from 'primeng/carousel';
 import { Projeto } from '../../interfaces/projeto';
 import { ProjetosService } from '../../services/projetos.service';
 
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
 @Component({
   selector: 'app-projetos',
-  imports: [ CardProjectComponent, CommonModule, CarouselModule],
+  imports: [ CardProjectComponent, CommonModule, CarouselModule, ProgressSpinnerModule],
   templateUrl: './projetos.component.html',
   styleUrl: './projetos.component.css'
 })
 export class ProjetosComponent implements OnInit {
   projetos: Projeto[] = []
-
+  loading: boolean = false
   constructor(private projetosService: ProjetosService) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    
     this.projetosService.getProjects().subscribe({
-      next: (projetos) => this.projetos = projetos,
-      error: (err) => console.error('Erro ao buscar projetos', err),
+      next: (projetos) => {this.projetos = projetos; this.loading = false},
+      error: (err) => {console.error('Erro ao buscar projetos', err); this.loading = false},
     });
   }
 
